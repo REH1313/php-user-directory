0abb48ea73c642909cc162a66634b896a6719396
Rebeca <rebhernandez144@gmail.com>
2025-11-02 22:56:25 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'sherd_reh144';
 $username = 'reh144';
-$password = '^pgzU%;d}}h9';
+$password = 'Abba_Father7';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
