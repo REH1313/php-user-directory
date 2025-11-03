345af75e71d1e065ede18cb7592e50dfb2e00e5a
Rebeca <rebhernandez144@gmail.com>
2025-11-02 23:31:37 -0600

    Update userView.php

--- userView.php
+++ userView.php
@@ -4,14 +4,6 @@ $users = getAllUsers();
 include 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Directory</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container mt-5">
     <h2>User Directory</h2>
     <?php if ($users): ?>
         <table class="table table-bordered">
@@ -37,6 +29,3 @@ include 'header.php';
     <?php endif; ?>
 
     <?php include 'footer.php';?>
-</div>
-</body>
-</html>
